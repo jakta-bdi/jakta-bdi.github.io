78266453f90e4f7c27b3195c60506dff0bb9c784
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2025-03-17 18:01:59 +0100

    fix: broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started">
+            to="/docs/getting-started/configuration">
               Get Started
           </Link>
         </div>
